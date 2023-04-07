08a0df87c06c99a93e85c623e773d0623965812d
HATOZ237 <58201604+HATOZ237@users.noreply.github.com>
2023-04-07 17:13:26 -0400

    created item for restaurant list

---
+++ app/src/main/java/ca/ulaval/ima/mp/utilities/GPS.kt
@@ -0,0 +1,10 @@
+package ca.ulaval.ima.mp.utilities
+
+import android.location.Location
+import android.location.LocationListener
+
+class GPS:LocationListener {
+    override fun onLocationChanged(p0: Location) {
+        TODO("Not yet implemented")
+    }
+}
