7665ae49755a3dc35201774116ea81032f3d4dc8
ledjex99 <74386020+ledjex99@users.noreply.github.com>
2023-04-19 03:31:40 -0400

    Map for Restaurant

--- app/src/main/java/ca/ulaval/ima/mp/ui/home/HomeFragment.kt
+++ app/src/main/java/ca/ulaval/ima/mp/ui/home/HomeFragment.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import ca.ulaval.ima.mp.databinding.FragmentHomeBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentHomeBinding? = null
+    private lateinit var mapView: MapView
+    private lateinit var googleMap: GoogleMap
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,16 +29,17 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+      //  val homeViewModel =
+        //    ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        mapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
+        mapView.onResume()
+        mapView.getMapAsync(this)
+
         return root
     }
 
@@ -39,4 +47,20 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    override fun onMapReady(map: GoogleMap) {
+
+        googleMap = map
+
+        // Customize the map
+        googleMap.uiSettings.isZoomControlsEnabled = true
+        googleMap.uiSettings.isMyLocationButtonEnabled = true
+
+        // Add a marker in Sydney and move the camera
+        val quebec = LatLng(46.8139, -71.2080)
+        googleMap.addMarker(MarkerOptions().position(quebec).title("Marker in Quebec"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(quebec))
+    }
+
+
 }
